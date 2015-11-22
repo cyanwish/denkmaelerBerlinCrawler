1c79ee297f2110f61ce38d115814cd6142f52e43
Robert_M <cyanwish@gmx.de>
2015-11-22 16:52:18 +0100

    minor changes
    added stuff for checking new keys, better structure, not too important

--- index.php
+++ index.php
@@ -8,40 +8,72 @@ require 'crawler.php';
  *  /// The main file /// 
  */
 
-$log = new Logging();
-$log->lfile('denkmaeler_log.txt'); 
-
+/////////start setup/////////////
 $crawler = new Crawler();
 $objectIds = $crawler->fetchObjectIds("denkmalliste.txt", '/090[0-9]{5}/');
-$length = count($objectIds);
-
+$storage = new Storage();
 //libxml error handling
 libxml_use_internal_errors(true);
+/////////finish setup/////////////
 
-//$objectId = "09011394";
-//$detailLink = getDetailLink($objectId);
-//$object = getObjectData($detailLink);
+$objectId = "09075277";
+$object = crawlObject($crawler, $storage, $objectId);
+//crawlAllObjects($crawler, $storage, $objectIds);
 
-$storage = new Storage();
+var_dump($object);
 
-// get the detaillink & object details for each objectid & save them to the db
-for ($i = 4000; $i < $length; $i++) { // 68,73
-    $detailLink = $crawler->getDetailLink($objectIds[$i]);
-    if ($detailLink == NULL) {
-        $log->lwrite($objectIds[$i] . 'Crawler Error. Unable to get the detail-link.');
-        sleep(1);
-    } else {
-        $object = $crawler->getObjectData($detailLink);
-        if ($object == NULL) {
-            $log->lwrite($objectIds[$i] . 'Crawler Error. Unable to get the object.');
+///////functions///////
+function crawlObject($crawler, $storage, $objectId){
+    $detailLink = $crawler->getDetailLink($objectId);
+    $object = $crawler->getObjectData($detailLink);
+    return $object;
+}
+
+function crawlAllObjects($crawler, $storage, $objectIds){
+    
+    $log = new Logging();
+    $log->lfile('denkmaeler_log.txt'); 
+    
+    $length = count($objectIds);
+
+    // get the detaillink & object details for each objectid & save them to the db
+    for ($i = 5000; $i < $length; $i++) { // 68,73
+        $detailLink = $crawler->getDetailLink($objectIds[$i]);
+        if ($detailLink == NULL) {
+            $log->lwrite($objectIds[$i] . 'Crawler Error. Unable to get the detail-link.');
             sleep(1);
         } else {
-            $result = $storage->saveObject($object);
-            echo  "||| " . round(($i / $length * 100), 2) . "% |||" . $object["Obj.-Dok.-Nr."] . " " . $object["Name"] . "\n";
-            sleep(1);
-        }
+            $object = $crawler->getObjectData($detailLink);
+            if ($object == NULL) {
+                $log->lwrite($objectIds[$i] . 'Crawler Error. Unable to get the object.');
+                sleep(1);
+            } else {
+                //$result = $storage->saveObject($object);
+                $keys = array_keys($object);
+                for ($x = 0; $x < count($keys); $x++){
+                    if( $keys[$x] != 'obj_nr' &&
+                        $keys[$x] != 'name' &&
+                        $keys[$x] != 'descr' &&
+                        $keys[$x] != 'picture' &&
+                        $keys[$x] != 'district' &&
+                        $keys[$x] != 'sub_district' &&
+                        $keys[$x] != 'p_concept' &&     
+                        $keys[$x] != 'p_builder' &&
+                        $keys[$x] != 'p_exec' &&
+                        $keys[$x] != 'nr' &&
+                        $keys[$x] != 'type' &&
+                        $keys[$x] != 'monument_notion' &&
+                        $keys[$x] != 'date' &&
+                        $keys[$x] != 'street') {
+                        echo 'neuer Key:' . $keys[$x] . "\n"; //Just to find new structures
+                    }
+                }
+                //progress-output
+                //echo  "||| " . round(($i / $length * 100), 2) . "% |||" . $object['obj_nr'] . " " . $object['name'] . "\n";
+                echo '|' . $object['date'];
+                sleep(1);
+            }
+        } 
     }
+    $log->lclose();   
 }
-$log->lclose();
-
-// TODO: picURL, Beschreibung, multiple Straßen fetchen
